85ec0bdf0a2b5acf4f65c52f2f4e2b668ac976ee
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-08 22:47:48 +0200

    change link path to take user directly on login after create an account

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+import { getAuth } from 'firebase/auth';
 import React from 'react'
+import { app } from './lib/firebase';
 
 export default function Dashboard() {
+  
+  const auth = getAuth(app);
+  const user =auth.currentUser
+  
   return (
     <div  className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="mb-2 text-2xl font-bold text-orange-500 sm:text-2xl md:text-3xl lg-text-4xl">Hello,</h1>
+      <h1 className="mb-2 text-2xl font-bold text-orange-500 sm:text-2xl md:text-3xl lg-text-4xl">Hello,{user?.email}</h1>
       <p className="text-lg text-gray-600">Welcome to your Firebase CRUD Task App!</p>
     </div>
   )
