85d44aa8adfb7ce37e7cad2fd8c3cb58d7ce44c4
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-08 20:44:04 +0200

    add logic to login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,38 +1,61 @@
+'use client'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {app} from '@/app/lib/firebase';
 import Link from 'next/link';
-import React from 'react'
-interface Login { 
-    id: string; 
-    title: string;
-     description: string; 
-     completed: boolean;
-      priority: "Low" | "Medium" | "High"; 
-      userEmail: string; }
-export default function Login() {
-  return (
-    <div className='bg-white flex flex-col min-h-screen justify-center items-center p-2'>
-        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-orange-500 mb-2">Login Page</h1>
-        <form className='flex flex-col  bg-gray-200 w-80 sm:w-60 h-80 p-4 m-4 rounded-lg gap-4'>
-            <input
-           type='email'
-           placeholder='UserEmail...'
-           required
-           className='text-gray-600  text-center bg-orange-300 rounded-lg w-full py-3 px-2'/>
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
-           <input
-           type='password'
-           placeholder='UserPassword...'
-           required
-           className='text-gray-600  text-center  bg-orange-300 rounded-lg w-full p-5 '/>
+interface Login { 
+     email:string
+    password:string|number}
+    
+export default function Login() {
+  const router = useRouter();
+  const auth= getAuth(app);
+
+  const[email,setEmail]=useState("");
+  const [password,setPassword] = useState("");
+  
+  async function handleLogin (event:React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth,email,password)
+      router.push("/dashboard")
+    } catch (error) {
+      alert(error)
+    }
+  }
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen p-2 bg-white'>
+        <h1 className="mb-2 text-2xl font-bold text-orange-500 sm:text-2xl md:text-3xl lg:text-4xl">Login Page</h1>
+        <form 
+           onSubmit={handleLogin}
+           className='flex flex-col gap-4 p-4 m-4 bg-gray-200 rounded-lg w-80 sm:w-60 h-80'>
+          <input
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          type='email'
+          placeholder='UserEmail...'
+          required
+          className='w-full px-2 py-3 text-center text-gray-600 bg-orange-300 rounded-lg'/>
+
+          <input
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          type='password'
+          placeholder='UserPassword...'
+          required
+          className='w-full p-5 text-center text-gray-600 bg-orange-300 rounded-lg '/>
            
-           <button
-           className="btn bg-orange-500 hover:bg-orange-600 text-white rounded-lg p-2 cursor-pointer" 
-           type="submit">Submit
-           </button>
+          <button
+          className="p-2 text-white bg-orange-500 rounded-lg cursor-pointer btn hover:bg-orange-600" 
+          type="submit">Submit
+          </button>
           
-        <div className="text-center text-gray-600 mt-2">
+    <div className="mt-2 text-center text-gray-600">
           <p>Don’t have an account?</p>
-          <Link href='/register' className="text-orange-500 hover:underline cursor-pointer">Register</Link>
-        </div>
+          <Link href='/register' className="text-orange-500 cursor-pointer hover:underline">Register</Link>
+    </div>
 
            
         </form>
