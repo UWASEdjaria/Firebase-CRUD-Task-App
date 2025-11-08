ce452d116645bf373ec3570be9c350821f2127e5
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-08 21:30:39 +0200

    add register logic

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,47 +1,77 @@
+'use client'
+import { app } from "@/app/lib/firebase";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import Link from "next/link";
-import React from "react";
+import {useRouter } from "next/navigation";
+import React, { useState } from "react";
+
 
 export default function Register() {
+    const router = useRouter();
+    const auth = getAuth(app);
+
+    const [email ,setEmail] = useState("");
+    const [password ,setPassword] =useState("")
+    const [ConfirmPassword ,setConfirmPassword]= useState("")
+    const [error ,setError] = useState("")
+    
+    async function handleRegister(event:React.FormEvent<HTMLFormElement>){
+       event.preventDefault();
+       setError("")
+       if(ConfirmPassword!== password){
+        setError('Passwords do not match')
+       }
+    try {
+       await createUserWithEmailAndPassword(auth,email,password)
+          router.push("/dashboard")
+    } catch (error) {
+      alert(error)
+    }
+   }
+   
   return (
-    <div className="bg-white min-h-screen  flex flex-col  justify-center items-center p-4 sm:p-2">
-      <h1 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold text-orange-500 mb-6">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white sm:p-2">
+      <h1 className="mb-6 text-2xl font-bold text-orange-500 sm:text-3xl md:text-3xl lg:text-4xl">
         Register Page
       </h1>
 
-      <form className="flex flex-col  bg-gray-200 w-72 sm:w-80 p-6 sm:p-3 rounded-lg gap-4">
-        <input
-          type="text"
-          placeholder="Full Name..."
-          required
-          className="text-gray-700 text-center bg-orange-300 rounded-lg w-full p-3 focus:outline-none focus:ring-2 focus:ring-orange-400"
-        />
+      <form
+          onSubmit={handleRegister}
+          className="flex flex-col gap-4 p-6 bg-gray-200 rounded-lg w-72 sm:w-80 sm:p-3">
+       
         <input
           type="email"
           placeholder="Email..."
           required
-          className="text-gray-700 text-center bg-orange-300 rounded-lg w-full p-3 focus:outline-none focus:ring-2 focus:ring-orange-400"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          className="w-full p-3 text-center text-gray-700 bg-orange-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
         <input
           type="password"
           placeholder="Password..."
           required
-          className="text-gray-700 text-center bg-orange-300 rounded-lg w-full p-3 focus:outline-none focus:ring-2 focus:ring-orange-400"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          className="w-full p-3 text-center text-gray-700 bg-orange-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
         <input
           type="password"
           placeholder="Confirm Password..."
           required
-          className="text-gray-700 text-center bg-orange-300 rounded-lg w-full p-3 focus:outline-none focus:ring-2 focus:ring-orange-400"
+          value={ConfirmPassword}
+          onChange={(e)=>setConfirmPassword(e.target.value)}
+          className="w-full p-3 text-center text-gray-700 bg-orange-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
         <button
           type="submit"
-          className="btn bg-orange-500 hover:bg-orange-600 text-white rounded-lg p-2 mt-2"
+          className="p-2 mt-2 text-white bg-orange-500 rounded-lg btn hover:bg-orange-600"
         >
           Register
         </button>
       </form>
 
-      <p className="text-center text-gray-600 mt-4">
+      <p className="mt-4 text-center text-gray-600">
         Already have an account?{" "}
         <Link href="/login" className="text-orange-500 hover:underline">
           Login
