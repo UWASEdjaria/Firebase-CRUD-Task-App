8be7248edd69b0a819e93e3498c7270261f64b34
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-09 20:14:57 +0200

    refactor the tasks logic

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,38 +1,7 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
+import { NextRequest,NextResponse } from "next/server";
 
-import { db } from '../lib/firebase';
-import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
-import { NextRequest,NextResponse } from 'next/server';
-interface Task {
-   id: string; 
-   title: string; 
-   description: string; 
-   completed: boolean; 
-   priority: "Low" | "Medium" | "High"; 
-   userEmail: string; }
-
-
-
-   const col = collection (db,"items");
-export async function POST(req:NextRequest){
-  try {
-     const data = await req.json();
-     await addDoc(col,data);
-     return NextResponse.json({message:"Item added successfully!" })
-  } catch (error) {
-    return  NextResponse.json({error:"Failed to add item",details:error})
-  }
-}
-
-
-export async function GET() {
-  try {
-    const snapshot = await getDocs(col);
-    const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    return NextResponse.json(items);
-  } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch items", details: error });
-  }
-}
 export async function PUT(req: NextRequest) {
   try {
     const { id, ...data } = await req.json();
---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,35 @@
+
+import { db } from '@/app/lib/firebase';
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
+import { NextRequest,NextResponse } from 'next/server';
+interface Task {
+   id: string; 
+   title: string; 
+   description: string; 
+   completed: boolean; 
+   priority: "Low" | "Medium" | "High"; 
+   userEmail: string; }
+
+
+
+   const col = collection (db,"items");
+export async function POST(req:NextRequest){
+  try {
+     const data = await req.json();
+     await addDoc(col,data);
+     return NextResponse.json({message:"Item added successfully!" })
+  } catch (error) {
+    return  NextResponse.json({error:"Failed to add item",details:error})
+  }
+}
+
+
+export async function GET() {
+  try {
+    const snapshot = await getDocs(col);
+    const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    return NextResponse.json(items);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch items", details: error });
+  }
+}
