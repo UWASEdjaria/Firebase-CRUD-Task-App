40b804999843442129d42034c1029e5b09c388e3
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-09 14:38:42 +0200

    update the tasks logic and add put and delete logic

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,22 +1,29 @@
-import { NextRequest, NextResponse } from "next/server";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import { db } from "../lib/firebase";
+
+import { db } from '../lib/firebase';
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
+import { NextRequest,NextResponse } from 'next/server';
+interface Task {
+   id: string; 
+   title: string; 
+   description: string; 
+   completed: boolean; 
+   priority: "Low" | "Medium" | "High"; 
+   userEmail: string; }
 
 
-const col = collection(db, "items"); // reference ya collection
 
-// CREATE - POST
-export async function POST(req: NextRequest) {
+   const col = collection (db,"items");
+export async function POST(req:NextRequest){
   try {
-    const data = await req.json();
-    await addDoc(col, data);
-    return NextResponse.json({ message: "Item added successfully!" });
+     const data = await req.json();
+     await addDoc(col,data);
+     return NextResponse.json({message:"Item added successfully!" })
   } catch (error) {
-    return NextResponse.json({ error: "Failed to add item", details: error });
+    return  NextResponse.json({error:"Failed to add item",details:error})
   }
 }
 
-// READ - GET
+
 export async function GET() {
   try {
     const snapshot = await getDocs(col);
@@ -24,5 +31,24 @@ export async function GET() {
     return NextResponse.json(items);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch items", details: error });
+  }
+}
+export async function PUT(req: NextRequest) {
+  try {
+    const { id, ...data } = await req.json();
+    const docRef = doc(db, "items", id);
+    await updateDoc(docRef, data);
+    return NextResponse.json({ message: "Item updated successfully!" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update item", details: error });
+  }
+}
+export async function DELETE(req: NextRequest) {
+  try {
+    const { id } = await req.json();
+    await deleteDoc(doc(db, "items", id));
+    return NextResponse.json({ message: "Item deleted!" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to delete", details: error });
   }
 }
